7f74ce44841eb1ea3718ad6a7999d06e130326dd
lastsoup <qingtang166@qq.com>
2018-11-09 17:33:12 +0800

    add

--- demo/components/demo/StaticTables.vue
+++ demo/components/demo/StaticTables.vue
@@ -1,5 +1,243 @@
 <template>
-  <div class="header">
-      Static Tables
+  <div>
+    <div class="table-responsive">
+					                    <table class="table table-striped">
+					                        <thead>
+					                            <tr>
+					                                <th>Invoice</th>
+					                                <th>User</th>
+					                                <th>Amount</th>
+					                            </tr>
+					                        </thead>
+					                        <tbody>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53451</a></td>
+					                                <td>Scott S. Calabrese</td>
+					                                <td>$24.98</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53452</a></td>
+					                                <td>Teresa L. Doe</td>
+					                                <td>$564.00</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53453</a></td>
+					                                <td>Steve N. Horton</td>
+					                                <td>$58.87</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53454</a></td>
+					                                <td>Charles S Boyle</td>
+					                                <td>$97.50</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53455</a></td>
+					                                <td>Lucy Doe</td>
+					                                <td>$12.79</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53456</a></td>
+					                                <td>Michael Bunr</td>
+					                                <td>$249.99</td>
+					                            </tr>
+					                        </tbody>
+					                    </table>
+					                </div>
+    <div class="table-responsive">
+					                    <table class="table table-bordered">
+					                        <thead>
+					                            <tr>
+					                                <th class="text-center">#</th>
+					                                <th>User</th>
+					                                <th>Plan</th>
+					                            </tr>
+					                        </thead>
+					                        <tbody>
+					                            <tr>
+					                                <td class="text-center">1</td>
+					                                <td><a href="#" class="btn-link">Scott S. Calabrese</a></td>
+					                                <td><span class="label label-purple">Bussines</span></td>
+					                            </tr>
+					                            <tr>
+					                                <td class="text-center">2</td>
+					                                <td><a href="#" class="btn-link">Teresa L. Doe</a></td>
+					                                <td><span class="label label-info">Personal</span></td>
+					                            </tr>
+					                            <tr>
+					                                <td class="text-center">3</td>
+					                                <td><a href="#" class="btn-link">Steve N. Horton</a></td>
+					                                <td><span class="label label-warning">Trial</span></td>
+					                            </tr>
+					                            <tr>
+					                                <td class="text-center">4</td>
+					                                <td><a href="#" class="btn-link">Charles S Boyle</a></td>
+					                                <td><span class="label label-purple">Bussines</span></td>
+					                            </tr>
+					                            <tr>
+					                                <td class="text-center">5</td>
+					                                <td><a href="#" class="btn-link">Lucy Doe</a></td>
+					                                <td><span class="label label-warning">Trial</span></td>
+					                            </tr>
+					                            <tr>
+					                                <td class="text-center">6</td>
+					                                <td><a href="#" class="btn-link">Michael Bunr</a></td>
+					                                <td><span class="label label-info">Personal</span></td>
+					                            </tr>
+					                        </tbody>
+					                    </table>
+					                </div>
+
+   <div class="table-responsive">
+     <table class="table  table-condensed">
+					                        <thead>
+					                            <tr>
+					                                <th>Invoice</th>
+					                                <th>User</th>
+					                                <th>Amount</th>
+					                            </tr>
+					                        </thead>
+					                        <tbody>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53451</a></td>
+					                                <td>Scott S. Calabrese</td>
+					                                <td>$24.98</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53452</a></td>
+					                                <td>Teresa L. Doe</td>
+					                                <td>$564.00</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53453</a></td>
+					                                <td>Steve N. Horton</td>
+					                                <td>$58.87</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53454</a></td>
+					                                <td>Charles S Boyle</td>
+					                                <td>$97.50</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53455</a></td>
+					                                <td>Lucy Doe</td>
+					                                <td>$12.79</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53456</a></td>
+					                                <td>Michael Bunr</td>
+					                                <td>$249.99</td>
+					                            </tr>
+					                            <tr>
+					                                <td><a href="#fakelink" class="btn-link">Order #53451</a></td>
+					                                <td>Scott S. Calabrese</td>
+					                                <td>$24.98</td>
+					                            </tr>
+					                        </tbody>
+					                </table>
+   </div>
+   <div class="table-responsive">
+     <div class="pad-btm form-inline">
+					            <div class="row">
+					                <div class="col-sm-6 table-toolbar-left">
+					                    <button id="demo-btn-addrow" class="btn btn-purple"><i class="demo-pli-add"></i> Add</button>
+					                    <button class="btn btn-default"><i class="demo-pli-printer"></i></button>
+					                    <div class="btn-group">
+					                        <button class="btn btn-default"><i class="demo-pli-exclamation-circle"></i></button>
+					                        <button class="btn btn-default"><i class="demo-pli-recycling"></i></button>
+					                    </div>
+					                </div>
+					                <div class="col-sm-6 table-toolbar-right">
+					                    <div class="form-group">
+					                        <input id="demo-input-search2" type="text" placeholder="Search" class="form-control" autocomplete="off">
+					                    </div>
+					                    <div class="btn-group">
+					                        <button class="btn btn-default"><i class="demo-pli-download-from-cloud"></i></button>
+					                        <div class="btn-group open">
+					                            <button data-toggle="dropdown" class="btn btn-default dropdown-toggle">
+					                                <i class="demo-pli-gear"></i>
+					                                <span class="caret"></span>
+					                            </button>
+					                            <ul role="menu" class="dropdown-menu dropdown-menu-right">
+					                                <li><a href="#">Action</a></li>
+					                                <li><a href="#">Another action</a></li>
+					                                <li><a href="#">Something else here</a></li>
+					                                <li class="divider"></li>
+					                                <li><a href="#">Separated link</a></li>
+					                            </ul>
+					                        </div>
+					                    </div>
+					                </div>
+					            </div>
+					        </div>
+<table class="table table-striped">
+					                <thead>
+					                    <tr>
+					                        <th class="text-center">Invoice</th>
+					                        <th>User</th>
+					                        <th>Order date</th>
+					                        <th>Amount</th>
+					                        <th>Status</th>
+					                        <th>Tracking Number</th>
+					                    </tr>
+					                </thead>
+					                <tbody>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53431</a></td>
+					                        <td>Steve N. Horton</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 22, 2014</span></td>
+					                        <td>$45.00</td>
+					                        <td>
+					                            <div class="label label-table label-success">Paid</div>
+					                        </td>
+					                        <td>-</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53432</a></td>
+					                        <td>Charles S Boyle</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 24, 2014</span></td>
+					                        <td>$245.30</td>
+					                        <td>
+					                            <div class="label label-table label-info">Shipped</div>
+					                        </td>
+					                        <td><i class="demo-pli-mine"></i> CGX0089734531</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53433</a></td>
+					                        <td>Lucy Doe</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 24, 2014</span></td>
+					                        <td>$38.00</td>
+					                        <td>
+					                            <div class="label label-table label-info">Shipped</div>
+					                        </td>
+					                        <td><i class="demo-pli-mine"></i> CGX0089934571</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53434</a></td>
+					                        <td>Teresa L. Doe</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 15, 2014</span></td>
+					                        <td>$77.99</td>
+					                        <td>
+					                            <div class="label label-table label-info">Shipped</div>
+					                        </td>
+					                        <td><i class="demo-pli-mine"></i> CGX0089734574</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53435</a></td>
+					                        <td>Teresa L. Doe</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 12, 2014</span></td>
+					                        <td>$18.00</td>
+					                        <td>
+					                            <div class="label label-table label-success">Paid</div>
+					                        </td>
+					                        <td>-</td>
+					                    </tr>
+					                </tbody>
+					            </table>
+   </div>
+
   </div>
 </template>
+<style scoped>
+@import "https://lastsoup.github.io/Nifty/css/demo/nifty-demo-icons.min.css";
+@import "../css/StaticTables.css";
+</style>
