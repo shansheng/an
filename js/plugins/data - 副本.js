2e96d70ffa9b05867f5bb41a43d072474f4c1a64
lastsoup <qingtang166@qq.com>
2019-02-04 21:08:22 +0800

    'jwt'

--- js/plugins/data - 副本.js
+++ js/plugins/data - 副本.js
@@ -14774,7 +14774,3 @@ var administrative=[
         "name": "澳门特别行政区"
     }
 ];
-
-// var host="https://localhost:5001";
-var host="https://58.213.48.24:3005";
-//var host="http://127.0.0.1:8011";
