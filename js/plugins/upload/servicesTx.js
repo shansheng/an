06834548630c3362b77db7f5f4f0ae89fe6e4dda
lastsoup <qingtang166@qq.com>
2019-02-18 22:41:26 +0800

    add

--- js/plugins/upload/servicesTx.js
+++ js/plugins/upload/servicesTx.js
@@ -13,12 +13,16 @@
         getFileListUrl:parent.host+"/api/WebAPI/GetFileList",
         uploadFileUrl:parent.host+"/api/WebAPI/Upload",
         deleteFileUrl:parent.host+"/api/WebAPI/DeleteFile",
+        uploader:null,
         preview:{
+          src:null,
           imgWrapWidth:360,
           imgWrapHeight:360,
           jcrop_api:null,
           boundx:null,
-          boundy:null
+          boundy:null,
+          thumbnailWidth:180,
+          thumbnailHeight: 180
         },
         init:function(){
             this.clear();
@@ -41,12 +45,34 @@
           var el=$(this.id);
           //切换tab页
           $("a[action-type='switchTab']",el).click(function(){dom._switchTabClick(this,dom)});
-          $("a[action-button='okClick']",el).click(function(){
-            $(".t-upload-selected",el).click();
-          });
+          $("a[action-button='okClick']",el).click(function(){dom._saveTxImageClick(this,dom)});
           $("a[action-button='reloadClick']",el).click(function(){
             dom.clear();
           });
+        },
+        _saveTxImageClick:function(c,dom){
+            var p=dom.preview;
+            var jcrop_api=p.jcrop_api;
+            if (jcrop_api != null) {
+                var select = jcrop_api.tellSelect();
+                var dw =p.thumbnailWidth,dh=p.thumbnailHeight,
+                boundx=p.boundx,boundy=p.boundy;
+                var rx = dw / select.w;
+                var ry = dh / select.h;
+                var width = Math.round(rx * boundx);
+                var height = Math.round(ry * boundy);
+                var w = dw;
+                var h = dh;
+                var x = Math.round(rx * select.x);
+                var y = Math.round(ry * select.y);
+                //dom.uploader.data('tUpload').async.saveUrl ="";
+                dom.getBase64(p.src).then(function(base64){
+                    $("#testTX").attr("src",base64)
+                  },function(err){
+                      console.log(err);
+                  });      
+                //$(".t-upload-selected",el).click();
+            }
         },
         _switchTabClick:function(c,dom){
             var el=$(dom.id);
@@ -60,6 +86,103 @@
                 $("[node-type='local']",el).hide();
                 $("[node-type='photobook']",el).show();
             }
+        },
+        detectVerticalSquash:function(img) {
+            var alpha, canvas, ctx, data, ey, ih, iw, py, ratio, sy;
+            iw = img.naturalWidth;
+            ih = img.naturalHeight;
+            canvas = document.createElement("canvas");
+            canvas.width = 1;
+            canvas.height = ih;
+            ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+            data = ctx.getImageData(0, 0, 1, ih).data;
+            sy = 0;
+            ey = ih;
+            py = ih;
+            while (py > sy) {
+              alpha = data[(py - 1) * 4 + 3];
+              if (alpha === 0) {
+                ey = py;
+              } else {
+                sy = py;
+              }
+              py = (ey + sy) >> 1;
+            }
+            ratio = py / ih;
+            if (ratio === 0) {
+              return 1;
+            } else {
+              return ratio;
+            }
+        },
+        drawImageIOSFix:function(ctx, img, sx, sy, sw, sh, dx, dy, dw, dh){
+            var vertSquashRatio;
+            vertSquashRatio = this.detectVerticalSquash(img);
+            return ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh / vertSquashRatio);
+        },
+        getBase64:function(img){
+                var  resize=function(img,thumbnailWidth,thumbnailHeight){
+                var info, srcRatio, trgRatio;
+                info = {
+                    srcX: 0,
+                    srcY: 0,
+                    srcWidth: img.width,
+                    srcHeight:img.height
+                  };
+                  srcRatio =img.width / img.height;
+                  info.optWidth = thumbnailWidth;
+                  info.optHeight = thumbnailHeight;
+                  if ((info.optWidth == null) && (info.optHeight == null)) {
+                    info.optWidth = info.srcWidth;
+                    info.optHeight = info.srcHeight;
+                  } else if (info.optWidth == null) {
+                    info.optWidth = srcRatio * info.optHeight;
+                  } else if (info.optHeight == null) {
+                    info.optHeight = (1 / srcRatio) * info.optWidth;
+                  }
+                  trgRatio = info.optWidth / info.optHeight;
+                  if (img.height < info.optHeight || img.width < info.optWidth) {
+                    info.trgHeight = info.srcHeight;
+                    info.trgWidth = info.srcWidth;
+                  } else {
+                    if (srcRatio > trgRatio) {
+                      info.srcHeight = img.height;
+                      info.srcWidth = info.srcHeight * trgRatio;
+                    } else {
+                      info.srcWidth = img.width;
+                      info.srcHeight = info.srcWidth / trgRatio;
+                    }
+                  }
+                  info.srcX = (img.width - info.srcWidth) / 2;
+                  info.srcY = (img.height - info.srcHeight) / 2;
+                  return info;
+              }
+              var image = new Image();
+              image.crossOrigin = "*";
+              image.src = img;
+              var el=this;
+              var deferred=$.Deferred();
+              if(img){
+                image.onload =function (){
+                    var canvas, ctx, resizeInfo, thumbnail, _ref, _ref1, _ref2, _ref3;
+                    var resizeInfo=resize(image,120,120);
+                    if (resizeInfo.trgWidth == null) {
+                        resizeInfo.trgWidth = resizeInfo.optWidth;
+                    }
+                    if (resizeInfo.trgHeight == null) {
+                        resizeInfo.trgHeight = resizeInfo.optHeight;
+                    }
+                    canvas = document.createElement("canvas");
+                    ctx = canvas.getContext("2d");
+                    canvas.width = resizeInfo.trgWidth;
+                    canvas.height = resizeInfo.trgHeight;
+                    el.drawImageIOSFix(ctx, image, (_ref = resizeInfo.srcX) != null ? _ref : 0, (_ref1 = resizeInfo.srcY) != null ? _ref1 : 0, resizeInfo.srcWidth, resizeInfo.srcHeight, (_ref2 = resizeInfo.trgX) != null ? _ref2 : 0, (_ref3 = resizeInfo.trgY) != null ? _ref3 : 0, resizeInfo.trgWidth, resizeInfo.trgHeight);
+                    thumbnail = canvas.toDataURL("image/png");
+                    deferred.resolve(thumbnail);
+                }
+                return deferred.promise();
+              }
         },
         _sImage: function (url) {
           //图片加载
@@ -140,24 +263,10 @@
             var dom=this;
             var el=$(dom.id);
             var upload=$("input[type='file']",el);
+            dom.uploader=upload;
             var loadingDom= $("[node-type='local_loading']",el);
             var onSuccessUpload = function (e) {
-                alert(1);
-           
-                var jcrop_api=dom.preview.jcrop_api,boundx=dom.preview.boundx,boundy=dom.preview.boundy;
-                if (jcrop_api != null) {
-                 var select = jcrop_api.tellSelect();
-                 var dw = 180, dh = 180;
-                 var rx = dw / select.w;
-                 var ry = dh / select.h;
-                 var width = Math.round(rx * boundx);
-                 var height = Math.round(ry * boundy);
-                 var w = dw;
-                 var h = dh;
-                 var x = Math.round(rx * select.x);
-                 var y = Math.round(ry * select.y);
-                
-                }
+               alert("上传成功！")
             }
             var onSelectUpload = function (e) {
                 loadingDom.show();
@@ -166,6 +275,7 @@
                 reader.readAsDataURL(file);
                 reader.onload = function (e) {
                     var database64 = e.target.result;
+                    dom.preview.src=database64;
                     dom._sImage(database64);
                     loadingDom.hide();
                 }
